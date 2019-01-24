b5824c52875d98d36d303a0705ff4b80d1652bb9
Liam Wright <mail@liam-wright.co.uk>
2019-01-24 10:15:51 +0000

    Basic structure continuing development

---
+++ src/Dfc.ProviderPortal.CourseText/Interfaces/ICourseTextService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfc.ProviderPortal.CourseText.Interfaces
+{
+    public interface ICourseTextService
+    {
+        Task<IEnumerable<IAzureSearchCourseText>> FindCourseTextAzureSearchData(ILogger log);
+    }
+}
